5399db947967c9947a59697f1743cbd56b8b1104
João Cabral <42394954+C4BRALL@users.noreply.github.com>
2023-11-30 13:54:27 -0300

    chore: add commitlint ands eslinter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('PDV – Fast Food')
+    .setDescription('This is a sample server PDV. For this sample')
+    .setVersion('1.0')
+    .addTag('pedidos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.SERVER_PORT);
   console.table({ '💡': `http://localhost:${process.env.SERVER_PORT}` });
 }
