ded8e1d0cc1e11cdb4408a95fc8d379b214175d7
João Cabral <42394954+C4BRALL@users.noreply.github.com>
2023-12-02 18:12:33 -0300

    feat(pdv): ensure products entitiy

--- src/core/entities/pdv/product.ts
+++ src/core/entities/pdv/product.ts
@@ -1,11 +1,21 @@
-// {
-//   "name": "Combo Faminto",
-//   "description": "1x Smash burguer da casa, 1x Refrigerante Lata, 1x Batatas Frita",
-//   "created_at": "2023-12-01 22:22:37.018",
-//   "updated_at": "2023-12-01 22:22:37.018",
-//   "category_id": "41599936-c11b-41c7-9594-8025488ef4ca",
-//   "id": "9430a4c5-16c6-4fbc-ac0e-51f19c4df7db",
-//   "image": "data:image/png;base64,UklGRsqZAABXRUJQVlA4IL6ZAABwoQKdASoAAgwCPlEkj",
-//   "currency": "R$",
-//   "price": 39.99
-// }
+import { UUID } from 'crypto';
+
+export class Products {
+  'name': string;
+
+  'description': string;
+
+  'created_at': Date;
+
+  'updated_at': Date;
+
+  'category_id': UUID;
+
+  'id': UUID;
+
+  'image': string;
+
+  'currency': string;
+
+  'price': number;
+}
